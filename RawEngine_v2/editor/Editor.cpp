ce41d8cf797e120de06141a00329e7eff0b01711
Micha Verheij <michaverheij@gmail.com>
2026-09-13 13:26:54 +0200

    migrate to opengl 4.3

--- RawEngine_v2/editor/Editor.cpp
+++ RawEngine_v2/editor/Editor.cpp
@@ -47,7 +47,7 @@ namespace editor {
     bool Editor::InitGLFW(GLFWwindow*& outWindow) {
         glfwInit();
         glfwWindowHint(GLFW_SAMPLES, 4);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     #ifdef __APPLE__
